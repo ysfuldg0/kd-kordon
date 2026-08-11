8baf8e6369459391fdbc051ab446c84e421cbc55
ysfuldg0 <ysfuldg0@gmail.com>
2026-08-11 15:12:24 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,40 @@
-const CACHE_NAME = 'kd-max-v1';
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll([
-      './index.html',
-      './manifest.json'
-    ]))
+const CACHE_NAME = 'kd-kordon-v2';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const clonedResponse = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clonedResponse));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
